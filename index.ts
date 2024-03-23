420326b6965e1ebc9cd69ad3343539738bd3e6d8
MohammedDaniyalRaza <160598244+MohammedDaniyalRaza@users.noreply.github.com>
2024-03-23 17:33:41 +0500

    I Add Console In The Line of 51

--- index.ts
+++ index.ts
@@ -6,13 +6,13 @@ import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
   {
-    message: "Enter First Number Please: ",
+    message: "Enter Your First Number Please: ",
     type: "number",
     name: "FirstNumber",
   },
 
   {
-    message: "Enter Second Number Please: ",
+    message: "Enter Your Second Number Please: ",
     type: "number",
     name:"SecondNumber",
   },
@@ -41,10 +41,11 @@ else if(answer.Operator === "Percentage"){
   console.log(answer.FirstNumber / answer.SecondNumber * 100 + "%");
 }
 else{
-  console.log("PLEASE ENTER A VALID OPERATOR...");
+  console.log("PLEASE ENTER A VALID OPERATOR....");
 };
 
 // Percentage= 
 // Diffrence (Divide By) Orignal Value
 // and Then Multiply By 100
 /* 30/100 x100 = 30% */
+console.log("The End...")
